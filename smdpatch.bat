68567ad08b93867ef3888f8f62f98c80a85f2e2b
1suky1 <msutlar@gmail.com>
2017-05-11 13:45:09 +0200

    Updated script
    Iterates through subfolders now and replaces original files instead of making new ones.

--- smdpatch.bat
+++ smdpatch.bat
@@ -5,8 +5,10 @@ echo                          smdpatch.bat by Rottweiler
 echo                 https://github.com/Rottweiler/smdpatch-batch
 echo[
 echo --------------------------------------------------------------------------------
-FOR %%f IN (*.smd) DO (
-	echo Patching %%f
-	more +1 "%%f" > "smdpatch_%%f"
+FOR /R %%f IN (*.smd) DO (
+echo Patching %%f
+more +1 "%%f" > "%%f.temp"
+xcopy %%f.temp %%f /y
+del %%f.temp /f /q
 )
 pause
